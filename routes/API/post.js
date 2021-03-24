0ce114248c7e8a3d8c2f710766e00b13992b2069
Carolina R <carolinarizk@gmail.com>
2021-03-24 18:10:42 -0400

    finalized controller and api to post task

--- routes/API/post.js
+++ routes/API/post.js
@@ -3,10 +3,5 @@ const router = require("express").Router();
 
 router.route("/")
 .post(Post.addPost)
-.get(Post.getPosts);
-
-router.route("/:id")
-.get(Post.getPost)
-.delete(Post.deletePost);
 
 module.exports = router;
