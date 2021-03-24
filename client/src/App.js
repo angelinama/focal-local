64b66b4198aadcf25b4fd350fd3788ce9e0d8de4
banuakman <banu@asterya.com>
2021-03-24 19:25:53 -0400

    add PostTaskPagereact route

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Wrapper from "./components/Wrapper";
 import Welcome from "./pages/Welcome";
 import Start from "./pages/Start";
+import PostTaskPage from "./pages/PostTaskPage"
 
 const App = () => (
   <Router>
     <Wrapper>
       <Route exact path="/" component={Start} />
       <Route exact path="/tasks" component={Welcome} />
+      <Route exact path="/addtask" component={PostTaskPage} />
     </Wrapper>
   </Router>
 );
