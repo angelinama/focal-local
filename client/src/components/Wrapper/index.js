6a5290334b21de02d0960874cabf3dbcf39f047d
banuakman <banu@asterya.com>
2021-03-24 19:01:32 -0400

    add dynamic rendering with react router, close #12

---
+++ client/src/components/Wrapper/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css";
+
+function Wrapper({ children }) {
+  return <div className="wrapper">{children}</div>;
+}
+
+export default Wrapper;
---
+++ client/src/pages/Welcome.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Welcome = () => (
+  <div>
+    <h1>THIS IS WELCOME - GET A TASK PAGE</h1>
+    <p>here - add search for local tasks </p>
+    <p>here - tasks card </p>
+  </div>
+);
+
+export default Welcome;
