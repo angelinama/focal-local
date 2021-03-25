f16ecd2b1464ad07a9845e211315c8b36871f5a8
banuakman <banu@asterya.com>
2021-03-25 12:40:10 -0400

    change welcome page

--- client/src/pages/Welcome.js
+++ client/src/pages/Welcome.js
@@ -5,7 +5,7 @@ import NavTabs from "../components/NavBar";
 const Welcome = () => (
   <div>
     <NavTabs/>
-    <h1>THIS IS WELCOME - GET A TASK PAGE</h1>
+    <h1>THIS WILL BECOME OUR FUTURE WELCOME </h1>
     <p>here - add search for local tasks </p>
     <p>here - tasks card </p>
   </div>
