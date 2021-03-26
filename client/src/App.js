bd32ebd1588653a1ce0731b0fe98d1f3edc22414
Banu Akman <75440726+banuakman@users.noreply.github.com>
2021-03-26 12:36:00 -0400

    Revert "Revert "login function using local strategy""

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,21 @@ import Start from "./pages/Start";
 import GetTaskPage from "./pages/GetTaskPage";
 import PostTaskPage from "./pages/PostTaskPage";
 import "./App.css";
+import { useGlobalContext } from "./context/GlobalState";
+import AuthenticatedApp from "./components/AuthenticatedApp";
+import UnauthenticatedApp from "./components/UnauthenticatedApp";
 
-const App = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={Start} />
-      <Route exact path="/gettask" component={GetTaskPage} />
-      <Route exact path="/addtask" component={PostTaskPage} />
-    </Switch>
-  </Router>
-);
+function App() {
+  // <Router>
+  //   <Switch>
+  //     <Route exact path="/" component={Start} />
+  //     <Route exact path="/gettask" component={GetTaskPage} />
+  //     <Route exact path="/addtask" component={PostTaskPage} />
+  //   </Switch>
+  // </Router>
+
+  const [state, dispatch] = useGlobalContext();
+  return state.userToken ? <AuthenticatedApp /> : <UnauthenticatedApp />;
+}
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { GlobalProvider } from "./context/GlobalState";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GlobalProvider>
+      <App />
+    </GlobalProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
-
