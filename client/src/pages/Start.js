310454968d3b8cc68a1cda89cfc16eadd9a8e2e2
banuakman <banu@asterya.com>
2021-03-27 11:56:49 -0400

    add start page to UnauthenticatedApp

--- client/src/pages/Start.js
+++ client/src/pages/Start.js
@@ -1,23 +1,21 @@
 import React from "react";
-import NavBar from "../components/NavBar";
 import Wrapper from "../components/Wrapper"
-import Background from "../components/Background"
+import { Link } from "react-router-dom";
 import "../styles/Start.css";
 
 const Start = () => (
   <div className="text-center">
-    <NavBar />
-    <Background />
     <Wrapper>
-      <img src="FocalLocal-logo.png" alt="Local Focal Your Community Hub" />
       <h3 className="hello">Easiest way to engage your local community,</h3>
       <h5 className="start">Help others in your neighborhood</h5>
       <h5 className="start">Ask local help when you need</h5>
       <h5 className="start">Support small businesses</h5>
       <h5 className="start">Follow up local events</h5>
-      <button type="submit" className="btn btn-primary">
-        SIGN UP
-      </button>
+
+      <Link to="/sign">
+        <button type="button" className="btn btn-primary start">LOGIN</button>
+      </Link>
+
     </Wrapper>
   </div>
 );
