00c6995b4675472387316e7c3e3e5a1ad34f95c5
banuakman <banu@asterya.com>
2021-03-28 10:07:39 -0400

    add styling to animated cards

--- client/src/components/AnimatedCard/index.js
+++ client/src/components/AnimatedCard/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { useSpring, animated } from "react-spring";
 import "./styles.css";
 
@@ -11,18 +10,20 @@ const calc = (x, y) => [
 const trans = (x, y, s) =>
   `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
 
-function AnimatedCard() {
+function AnimatedCard({ children }) {
   const [props, set] = useSpring(() => ({
     xys: [0, 0, 1],
     config: { mass: 5, tension: 350, friction: 40 },
   }));
   return (
     <animated.div
-      class="card"
+      class="animatedcard"
       onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
       onMouseLeave={() => set({ xys: [0, 0, 1] })}
       style={{ transform: props.xys.interpolate(trans) }}
-    />
+    >
+      { children }
+    </animated.div>
   );
 }
 
