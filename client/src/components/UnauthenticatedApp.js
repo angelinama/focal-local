97fc6a8feae497dc1f207efef46811859b4cbad3
banuakman <banu@asterya.com>
2021-03-27 21:56:10 -0400

    finish styling start

--- client/src/components/UnauthenticatedApp.js
+++ client/src/components/UnauthenticatedApp.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import LoginEl from "./LogIn";
 import Start from "../pages/Start";
 import NavBar from "./NavBar";
+import Wrapper from "./Wrapper";
 import Background from "../components/Background";
 import "../styles/UnauthenticatedApp.css";
 function UnauthenticatedApp() {
@@ -13,7 +14,11 @@ function UnauthenticatedApp() {
       <Background />;
       <Wrapper>
         <div className="text-center">
-          <img src="FocalLocal-logo.png" alt="Local Focal Your Community Hub" />
+          <img
+            className="startlogo"
+            src="FocalLocal-logo.png"
+            alt="Local Focal Your Community Hub"
+          />
         </div>
         {isStartPage ? (
           <Start switchToLogin={() => setIsStartPage(false)} />
