4dd804039b7b0e832940dd3a60c3bf92fa7fd42a
banuakman <banu@asterya.com>
2021-03-29 08:34:26 -0400

    delete switch

--- client/src/components/AuthenticatedApp.js
+++ client/src/components/AuthenticatedApp.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import GetTaskPage from "../pages/GetTaskPage";
 import PostTaskPage from "../pages/PostTaskPage";
 import TaskDetailsPage from "../pages/TaskDetailsPage";
